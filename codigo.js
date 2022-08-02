a09ba6dfa8354170c4bf6a9b9d7dfcc35f8aa29f
angieeferra <tubinob@gmail.com>
2022-08-02 19:21:54 -0300

    primer entrega

--- codigo.js
+++ codigo.js
@@ -10,17 +10,41 @@ let anio = parseInt(tercerNumero);
 
 /*FUNCIONES*/
 
-function sumar (a, b) {
-    var suma = a + b;
-    console.log("El resultado de " + a + " + " + b + " es : " + suma);
-}
+// function sumar (a, b) {
+//    let suma = a + b;
+ //   console.log("El resultado de " + a + " + " + b + " es : " + suma);
+// }
 
-sumar(5 , 3);
-sumar(8, 9);
+// sumar(5 , 3);
+// sumar(8, 9);
+
+/*funciones flecha*/ 
+
+const saludar = nombre => {console.log ("Hola como estas?" + nombre)}
+
+
 
 /*bucle while*/
 var contador = 0
-while(contador<50){
+while(contador<10){
     contador++;
     console.log(contador)
 }
+
+/*calculamos el costo de un producto con un 15% de descuento en compras */
+var cantidad;
+var precio;
+var descuento;
+var compra;
+var11 pagar;
+
+precio = parseFloat(prompt("Ingresar precio"));
+cantidad = parseFloat(prompt("Ingresar cantidad"));
+
+compra =  precio*cantidad;
+descuento = compra*0.15;
+pagar = compra-descuento;
+
+console.log("El descuento es: " + descuento);
+
+console.log("El total a pagar es: " + pagar)
